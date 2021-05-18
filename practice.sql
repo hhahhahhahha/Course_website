deb1ae5e2c0603a2c73c38ad97dbb35595fdfaec
chenyong <505797019@qq.com>
2021-05-18 16:38:14 +0800

    sql文件

---
+++ practice.sql
@@ -0,0 +1,222 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : chenyong
+ Source Server Type    : MySQL
+ Source Server Version : 80023
+ Source Host           : localhost:3306
+ Source Schema         : practice
+
+ Target Server Type    : MySQL
+ Target Server Version : 80023
+ File Encoding         : 65001
+
+ Date: 18/05/2021 16:37:38
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for data
+-- ----------------------------
+DROP TABLE IF EXISTS `data`;
+CREATE TABLE `data`  (
+  `id` int NOT NULL,
+  `flag` int NULL DEFAULT NULL,
+  `number` int NULL DEFAULT NULL,
+  `data` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of data
+-- ----------------------------
+INSERT INTO `data` VALUES (1, 1, 1, '恭喜数据结构课程获得国家一级课程');
+INSERT INTO `data` VALUES (2, 2, 1, '数据结构课程与3月1日开课');
+INSERT INTO `data` VALUES (3, 3, 1, '有道数据结构');
+
+-- ----------------------------
+-- Table structure for menu
+-- ----------------------------
+DROP TABLE IF EXISTS `menu`;
+CREATE TABLE `menu`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `pattern` varchar(128) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `parentId` int NULL DEFAULT NULL,
+  `enabled` tinyint NULL DEFAULT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `component` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 7 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of menu
+-- ----------------------------
+INSERT INTO `menu` VALUES (1, '/', NULL, 1, '所有', '');
+INSERT INTO `menu` VALUES (2, '/home', 1, 1, '所有管理', 'Home');
+INSERT INTO `menu` VALUES (3, '/db/**', 2, 1, '成绩管理', 'Score');
+INSERT INTO `menu` VALUES (4, '/admin/**', 2, 1, '基本信息', 'Information');
+INSERT INTO `menu` VALUES (5, '/student/**', 2, 1, '学生管理', 'Student');
+INSERT INTO `menu` VALUES (6, '/home', 1, 1, '权限管理', 'Home');
+INSERT INTO `menu` VALUES (7, '/user/**', 6, 1, '用户管理', 'UserManage');
+
+-- ----------------------------
+-- Table structure for menu_role
+-- ----------------------------
+DROP TABLE IF EXISTS `menu_role`;
+CREATE TABLE `menu_role`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `mid` int NULL DEFAULT NULL,
+  `rid` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of menu_role
+-- ----------------------------
+INSERT INTO `menu_role` VALUES (1, 3, 1);
+INSERT INTO `menu_role` VALUES (2, 4, 2);
+INSERT INTO `menu_role` VALUES (3, 5, 3);
+INSERT INTO `menu_role` VALUES (4, 7, 2);
+
+-- ----------------------------
+-- Table structure for role
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `nameZh` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES (1, 'ROLE_dba', '数据库管理员');
+INSERT INTO `role` VALUES (2, 'ROLE_admin', '系统管理员');
+INSERT INTO `role` VALUES (3, 'ROLE_user', '用户');
+
+-- ----------------------------
+-- Table structure for student
+-- ----------------------------
+DROP TABLE IF EXISTS `student`;
+CREATE TABLE `student`  (
+  `uid` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `age` int NULL DEFAULT NULL,
+  `sex` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`uid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 40 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = COMPACT;
+
+-- ----------------------------
+-- Records of student
+-- ----------------------------
+INSERT INTO `student` VALUES (31, 'hello', 23, '男');
+INSERT INTO `student` VALUES (32, 'llll', 22, '男');
+
+-- ----------------------------
+-- Table structure for student_copy
+-- ----------------------------
+DROP TABLE IF EXISTS `student_copy`;
+CREATE TABLE `student_copy`  (
+  `uid` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `age` int NULL DEFAULT NULL,
+  `sex` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`uid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 28 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = COMPACT;
+
+-- ----------------------------
+-- Records of student_copy
+-- ----------------------------
+INSERT INTO `student_copy` VALUES (23, '徐昊', 22, '男');
+INSERT INTO `student_copy` VALUES (25, '阿旺', NULL, NULL);
+INSERT INTO `student_copy` VALUES (27, '信息', 35, 'VC');
+INSERT INTO `student_copy` VALUES (28, '徐哈', 22, '男');
+
+-- ----------------------------
+-- Table structure for student_count
+-- ----------------------------
+DROP TABLE IF EXISTS `student_count`;
+CREATE TABLE `student_count`  (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `count` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = COMPACT;
+
+-- ----------------------------
+-- Records of student_count
+-- ----------------------------
+INSERT INTO `student_count` VALUES (1, 2);
+
+-- ----------------------------
+-- Table structure for teacherdata
+-- ----------------------------
+DROP TABLE IF EXISTS `teacherdata`;
+CREATE TABLE `teacherdata`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci NULL DEFAULT NULL,
+  `number` varchar(13) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `major` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `job_title` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `email` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of teacherdata
+-- ----------------------------
+INSERT INTO `teacherdata` VALUES (3, '陈浩', '2018051', '软件工程', '教授', '11111@11');
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`  (
+  `id` int NOT NULL AUTO_INCREMENT COMMENT 'userID',
+  `name` varchar(32) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '姓名',
+  `phone` char(11) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '手机号码',
+  `telephone` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '住宅电话',
+  `address` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '联系地址',
+  `enabled` tinyint(1) NULL DEFAULT 1,
+  `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '用户名',
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `remark` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 12 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = COMPACT;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, '超级管理员', '18568887789', '029-82881234', '哈尔滨', 1, 'admin', '$2a$10$rHGbs9itDAGHpRoR387tmuiJ3qC47qo9hagEyMHyxA8aSLUD6nvy6', NULL);
+INSERT INTO `user` VALUES (2, '徐昊', '18568123489', '029-82123434', '重庆', 1, 'xuhao', '$2a$10$rHGbs9itDAGHpRoR387tmuiJ3qC47qo9hagEyMHyxA8aSLUD6nvy6', NULL);
+INSERT INTO `user` VALUES (3, '老王', '18568123666', '029-82111555', '广州', 1, 'laowang', '$2a$10$oE39aG10kB/rFu2vQeCJTu/V/v4n6DRR0f8WyXRiAYvBpmadoOBE.', NULL);
+INSERT INTO `user` VALUES (8, '老张', NULL, NULL, NULL, 1, 'laozhang', '$2a$10$lv5kCzOsIVXVrYSOxykyGOUPpW.AIJ9.rAIVk6EJ6nxOyFGVmC0Zq', NULL);
+INSERT INTO `user` VALUES (9, '老李', NULL, NULL, NULL, 0, 'laoli', '$2a$10$s3.qcTY9tw9egA.UoyBZ8Oyi6.NVAW84P6mN.A8wcgzXJQho.Ck9i', NULL);
+INSERT INTO `user` VALUES (10, '小程', NULL, NULL, NULL, 1, 'xiaocheng', '$2a$10$9WvViONP7a.VcId3ktgg.Odj.WJExpc./j/eSNv2bSa8nArwfGGY2', NULL);
+INSERT INTO `user` VALUES (11, '哈哈哈', NULL, NULL, NULL, 1, 'hhh', '$2a$10$rHGbs9itDAGHpRoR387tmuiJ3qC47qo9hagEyMHyxA8aSLUD6nvy6', NULL);
+INSERT INTO `user` VALUES (12, '晨晨', '1838261745', NULL, '重庆市', 1, 'chenchen', '$2a$10$vNxILXyJb6/gSZtHUUrvdOS4qzJ.Z4UT1x7ZxfqNao75H5vFsduO.', NULL);
+
+-- ----------------------------
+-- Table structure for user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `uid` int NULL DEFAULT NULL,
+  `rid` int NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = DYNAMIC;
+
+-- ----------------------------
+-- Records of user_role
+-- ----------------------------
+INSERT INTO `user_role` VALUES (1, 1, 1);
+INSERT INTO `user_role` VALUES (2, 1, 2);
+INSERT INTO `user_role` VALUES (3, 2, 2);
+INSERT INTO `user_role` VALUES (4, 3, 3);
+INSERT INTO `user_role` VALUES (5, 1, 3);
+
+SET FOREIGN_KEY_CHECKS = 1;
